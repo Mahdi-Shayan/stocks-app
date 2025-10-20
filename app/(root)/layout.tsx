8814ac89f75d2081679723a00eb01a7295884b4a
Mahdi-Shayan <mahdieshayan4030@gmail.com>
2025-10-20 08:26:11 -0700

    get start

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export default function layout({ children }: { children: ReactNode }) {
+  return (
+    <main className="min-h-screen text-gray-400">
+      <div className="container py-10">{children}</div>
+    </main>
+  );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+function Home() {
+  return (
+    <div className="flex home-wrapper min-h-screen">
+      
+    </div>
+  )
+}
+export default Home
